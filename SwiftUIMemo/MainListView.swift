2b54241fa8cf107a33c71727b154c89096246b9f
kimhyunjin <khjzzm@gmail.com>
2024-04-04 10:07:49 +0900

    Step1

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by hyunjin kim on 4/4/24.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
